0d3d20858aead01223bf0248f9e7851aeabeca4c
fistfvck <fistfvck@gmail.com>
2009-07-23 13:39:05 +0900

    add validates.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+  validates_presence_of :title, :description, :image_url
+  validates_numericality_of :price, :only_integer => true, :greater_than => 0
+  validates_uniqueness_of :title
+  validates_format_of :image_url, :with => /\.(gif|jpg|png)\Z/i
 end
